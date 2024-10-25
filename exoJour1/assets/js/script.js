c21501f9c456feccd6bb15dd9efc4b5fd01e3bb3
Kev <kevin@MacBook-Pro-de-Kev.local>
2024-10-25 10:04:18 +0200

    hidden files suppressed. It should be ok now

---
+++ exoJour1/assets/js/script.js
@@ -0,0 +1,177 @@
+////////////// 1. Boucle for : Afficher les nombres de 1 à 10
+
+
+// for (let i = 1; i<=10; i++) {
+//     console.log (i);
+// }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////// 2. Somme des nombres de 1 à 10 avec une boucle 
+
+// let num = 0;
+
+// for (let i = 1; i<=10; i++) {
+//     num = num + i
+// }
+// console.log('La somme est de : ' + num);
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////// 3. Calculer le périmètre d'un rectangle
+
+// let lenght = 20;
+// let width = 50;
+
+// console.log((lenght+width)*2)
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////// 4. Calculer le périmètre d'un rectangle
+
+// let num = 15
+
+// console.log('Nous allons vérifier si 15 est un multiple de 3 ou de 5');
+
+// if (num%3==0 || num%5==0) {
+//     console.log(num + ' est bien un multiple de 3 ou de 5');
+// } else {
+//     console/log ('Non !' + num + " n'est pas un multiple de 3 ou de 5");
+//
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////// 5. Calculer le périmètre d'un rectangle
+
+// const teamA = 56;
+// const teamB = 78;
+// const teamC = 45;
+// let meilleurScore = 0;
+
+// if (teamA > meilleurScore) {
+//     meilleurScore = teamA;
+// }
+
+// if (teamB > meilleurScore) {
+//     meilleurScore = teamB;
+// };
+// if (teamC > meilleurScore) {
+//     meilleurScore = teamC;
+// }
+// console.log("le meilleur score est bien celui de l'équipe qui a marqué " + meilleurScore + " points");
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////// 6. Trouver un nombre premier 
+/* J'ai fait des recherches sur mdn web docs car je ne savais plus comment faire 
+pour appeler une function*/
+
+// const primeNumber = 28 ;
+
+
+
+// function primeTest(primeNumber) {
+
+//     if (primeNumber <=1) {
+//         console.log('pas premier')
+//     }
+
+//     for (let i = 2; i<=Math.sqrt(primeNumber); i++) {
+
+//         if ((primeNumber%i)==0) {
+//             return false;
+//         } else {
+//             return true;
+//         }
+//     }
+// }
+
+// if (primeTest(primeNumber) === false) {
+//     console.log ("Ce n'est pas un nombre premier");
+// } else {
+//     console.log ("PRIMERO YAYAYAYAYAYA");
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////// 7. Trouver un nombre premier 
+
+// const num = 3;
+// let i = 3;
+
+// while (i<=10) {
+//     console.log(num*i);
+//     i++;
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////// 8. Trouver un nombre premier 
+
+// Utilisation de logarithme10 : d = log10(N) + 1 
+// --> ici 10puissance 4 = 10 000 (maximum pour 98765) + 1 = 5 = le nombre de chiffres ! 
+
+/* J'ai fait des recherches sur mdn web docs car je ne savais pas comment 
+faire pour revenir à l'entier le plus bas "floor" et le plus haut "round" */
+
+
+// let num = 98765;
+// let amount = Math.log10(98765) + 1;
+
+// console.log(Math.floor(amount));
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////// 9. Somme des chiffres d'un nombre 
+// Non terminé. J'ai trouvé une solution sur internet mais je ne comprends pas totalement alors je ne préfère pas l'utiliser. 
+
+// let num = prompt('rentre un nombre');
+
+// const list = num.toString()
+//                 .split('')            
+//                 .map(num)
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////// 10. Convertir des secondesen minutes et secondes
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////// 12. Trouver l'erreur
+
+
+// let i = 1;
+// while (i <= 10; i++) {
+//     console.log(i);
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////// 13 - Corriger l'erreur
+
+// let a = 5;
+// let b = 5;
+// if (a === b) {
+//     console.log ('Les nombres sont égaux');
+// } else {
+//     console.log('Les nombres sont différents');
+// } 
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////// 14 - Optimiser le code
+
+
+
+
+
